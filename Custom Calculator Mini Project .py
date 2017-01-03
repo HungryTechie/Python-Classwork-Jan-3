b8a48565babfbcb195e92376f13e4a95a3353c2e
Henry <Pvrpl3Afr0@users.noreply.github.com>
2017-01-03 14:56:55 -0500

    Added files
    bing bang boom

---
+++ Custom Calculator Mini Project .py
@@ -0,0 +1,33 @@
+#calculator mini projects
+
+#Calc. 1, Temp. converter
+
+Farenheit= input("Please type in a temperature in Farenheit: ")
+F= float(Farenheit)
+
+Celsius= F - 32 * 1.8
+
+print("The temperature in Celsius is: ", Celsius)
+
+
+#Calc. 2, Original problem?
+
+#Username generator, Takes first three letters of name, last name and age to
+#Generate username
+
+First= input("Please type in your First name: ")
+
+Last= input("Please type in your Last name: ")
+
+Last = a[0,1,2]
+
+Age= input("Please input your age: ")
+A= float(Age)
+
+#print(First(len(2)) + Last(len(2)) + A)
+#Come back to this
+
+'''
+In the end the program should display the following
+print("HenRam17")
+'''
